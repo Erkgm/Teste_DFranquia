ba83b2089d05b54d1cf9312fa0465751ae3a8de6
Erick <erickmaia134@gmail.com>
2026-05-08 21:24:18 -0300

    feat(veterinarian): CRUD completo do veterinario

--- src/Controller/VeterinarianController.php
+++ src/Controller/VeterinarianController.php
@@ -2,17 +2,81 @@
 
 namespace App\Controller;
 
+use App\Entity\Veterinarian;
+use App\Form\VeterinarianType;
+use App\Repository\VeterinarianRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
-final class VeterinarianController extends AbstractController
+#[Route('/veterinarios', name: 'veterinarian_')]
+class VeterinarianController extends AbstractController
 {
-    #[Route('/veterinarian', name: 'app_veterinarian')]
-    public function index(): Response
+    #[Route('', name: 'index', methods: ['GET'])]
+    public function index(VeterinarianRepository $repo, PaginatorInterface $paginator, Request $request) : Response
     {
-        return $this->render('veterinarian/index.html.twig', [
-            'controller_name' => 'VeterinarianController',
+        $query = $repo -> createQueryBuilder('v')
+            ->orderBy('v.name', 'ASC')
+            ->getQuery();
+
+        $pagination = $paginator -> paginate($query, $request -> query -> getInt('page', 1), 10);
+
+        return $this -> render('veterinarian/index.html.twig', [
+            'pagination' => $pagination,
         ]);
+    }
+
+    #[Route('/novo', name:'new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $em) : Response
+    {
+        $vet = new Veterinarian();
+        $form = $this->createForm(VeterinarianType::class, $vet);
+        $form -> handleRequest($request);
+
+        if($form -> isSubmitted() && $form -> isValid()) {
+            $em -> persist($vet);
+            $em -> flush();
+            $this -> addFlash('success', 'Veterinário cadastrado');
+            return $this -> redirectToRoute('veterinarian_index');
+        }
+
+        return $this -> render('veterinarian/form.html.twig', [
+            'form' => $form,
+            'title' => 'Novo Veterinário',
+        ]);
+    }
+
+    #[Route('/{id}/editar', name: 'edit', methods: ['GET', 'POST'])]
+    public function edit(Veterinarian $vet, Request $request, EntityManagerInterface $em) : Response
+    {
+        $form = $this -> createForm(VeterinarianType::class, $vet);
+        $form -> handleRequest($request);
+
+        if($form -> isSubmitted() && $form -> isValid()){
+            $em -> flush();
+            $this.$this->addFlash('success', 'Veterinário atualizado');
+            return $this ->redirectToRoute('veterinarian_index');
+        }
+
+        return $this -> render('veterinarian/index.html.twig', [
+            'form' => $form,
+            'title' => 'Editar Veterinário',
+            'vet' => $vet,
+        ]);
+    }
+
+    #[Route('/{id}/excluir', name: 'delete', methods: ['POST'])]
+    public function delete(Veterinarian $vet, Request $request, EntityManagerInterface $em) : Response
+    {
+        if($this -> isCsrfTokenValid('delte-vet-' . $vet -> getId(), $request -> request -> get('_token'))) {
+            $em -> remove($vet);
+            $em -> flush();
+            $this -> addFlash('success', 'Veterinário excluído');
+        }
+
+        return $this -> redirectToRoute('veterinarian_index');
     }
 }
--- src/Form/VeterinarianType.php
+++ src/Form/VeterinarianType.php
@@ -6,6 +6,8 @@ use App\Entity\Farm;
 use App\Entity\Veterinarian;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,14 +16,14 @@ class VeterinarianType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('crmv')
-            ->add('farms', EntityType::class, [
-                'class' => Farm::class,
-                'choice_label' => 'id',
-                'multiple' => true,
+            ->add('name', TextType::class, [
+                'label' => 'Nome',
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Nome Completo'],
             ])
-        ;
+            ->add('crmv', TextType::class, [
+                'label' => 'CRMV',
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Ex: GO-12345'],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
